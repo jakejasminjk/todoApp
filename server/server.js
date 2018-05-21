623b8a9c2e6f5a692ba0ee59b0ed09ca26433d3b
Kervens Jasmin <jakejasminjk@gmail.com>
2018-05-21 02:08:12 +0000

    added a GET route for all todos

--- server/server.js
+++ server/server.js
@@ -9,13 +9,21 @@ let app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => { //Resource creatio endpoint 
+app.post('/todos', (req, res) => { //Resource creation endpoint 
     let todo = new Todo({
         text: req.body.text
     });
     
     todo.save().then((doc) => {
         res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    })
+});
+
+app.get('/todos', (req,res) => {
+    Todo.find({}).then((todos) => {
+        res.send({todos}); //setting the todos as an object rather than an array(without the brackets it's an array)
     }, (err) => {
         res.status(400).send(err);
     })
